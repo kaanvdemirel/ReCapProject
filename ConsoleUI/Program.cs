74ede983693622053028573f16211b7566592412
kaan demirel <kaandemirel190314@gmail.com>
2024-07-19 16:09:56 +0300

    Updated crud operations for business layer. Added core layer.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,14 +3,6 @@ using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 
-CarManager carManager = new CarManager(new EfProductDal()) ;
-
-
-
-foreach (var car in carManager.GetByBrandId(5))
-{
-    Console.WriteLine(car.Name);
-}
 
 
 
